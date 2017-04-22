0cdf2b3eab4c88bf32bc4a490e854d95fecc4f2a
Piotr Król <piotr.krol@codesthq.com>
2017-04-22 16:54:43 +0200

    create PCA

---
+++ include/PCA.hpp
@@ -0,0 +1,18 @@
+#ifndef PCA_HPP
+#define PCA_HPP
+#include <RcppArmadillo.h>
+
+using namespace arma;
+using namespace std;
+
+class PCA
+{
+  public:
+    PCA(mat matrix);
+
+    vec calculate();
+
+    mat matrix;
+};
+
+#endif
---
+++ src/PCA.cpp
@@ -0,0 +1,17 @@
+#include "../include/PCA.hpp"
+
+PCA::PCA(mat matrix): matrix(matrix) {}
+
+vec PCA::calculate()
+{
+  vec transformedVector(matrix.n_rows);
+  vec eig_values;
+  mat eig_vectors;
+
+  eig_sym(eig_values, eig_vectors, cov(matrix));
+
+  for(int i=0; i<matrix.n_rows; i++)
+    transformedVector[i] = sum((matrix.row(i) - mean(matrix)) * eig_vectors.row(eig_vectors.n_rows - matrix.n_rows + i).t());
+  
+  return transformedVector;
+}
