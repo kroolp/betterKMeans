e927ff168e686969092374d7705d0eb1d7512db4
Piotr Król <piotr.krol@codesthq.com>
2017-06-13 20:48:08 +0200

    use the same function for plot

--- R/betterKMeans.R
+++ R/betterKMeans.R
@@ -25,6 +25,24 @@ plot.betterKmeans <- function(result, cex=.1, pch=19, xlab="X", ylab="Y", asp=1,
   draw_functions(result$pointsToDraw, k);
 }
 
+plot_result <- function(result, cex=.1, pch=19, xlab="X", ylab="Y", asp=1, xlim=c(0, 1.0), ylim=c(0, 1.0), type=NULL, main=NULL, sub=NULL) {
+  k = length(result$errors);
+  
+  plot_points(result$inputMatrix, result$labels, cex=cex, pch=pch, xlab=xlab, ylab=ylab, asp=asp, xlim=xlim, ylim=ylim, type=type, main=main, sub=sub);
+  draw_centers(result$centers, k);
+  
+  if(!is.null(result$eigenVectors))
+    draw_bases(result$eigenVectors, result$eigenValues, result$centers, k);
+  
+  if(!is.null(result$pointsToDraw))
+    draw_functions(result$pointsToDraw, k);
+  
+  if(!is.null(result$eigenVectors) && is.null(result$pointsToDraw))
+    abline(0,0);
+}
+
+plot.kmeans <- plot.kOmeans <- plot.betterKmeans <- plot_result;
+
 plot_points <- function(pointsMatrix, labels, cex, pch, xlab, ylab, asp, xlim, ylim, type, main, sub) {
   plot(pointsMatrix, col=labels, cex=cex, pch=pch, xlab=xlab, ylab=ylab, asp=asp, xlim=xlim, ylim=ylim, type=type, main=main, sub=sub);
 }
